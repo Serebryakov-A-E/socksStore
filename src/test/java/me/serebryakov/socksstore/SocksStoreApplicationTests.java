026668c8bd669e4622aee35238fe57d103d2262d
Alexey <ReetzCraft@gmail.com>
2023-02-17 14:40:01 +0300

    Initial commit

---
+++ src/test/java/me/serebryakov/socksstore/SocksStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package me.serebryakov.socksstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
